abf1e2568952a54361ba5393475f92662de28a5e
Alexandre <alexandre44.robin@gmail.com>
2018-08-13 18:05:29 +0200

    [Update] Updated the Welcome Text

--- src/states/note.js
+++ src/states/note.js
@@ -3,6 +3,7 @@ import { observable, action } from 'mobx'
 const WelcomeText = `h1. Welcome to Jira Text Editor
 
 Hi! I'm your first file in *JTE*. If you want to play, you can edit me.
+?? JTE ??
 
 h2. H2 Title
 h3. H3 Title
@@ -10,12 +11,34 @@ h4. H4 Title
 h5. H5 Title
 h6. H6 Title
 
+Superscript : ^I'm up here !^
+Subscript ~Look down !~
+{{monospaced}}
+
+{quote}
+    Would be cool if I could preview my stories :D
+{quote}
+
+{color:red}
+    look ma, red text!
+{color}
+
+|| Table ||
+|table|
+
+If you want to send some Pull Requests: [GitHub | https: //github.com/alexandrobin/jira-text-editor]
+:) :D :P (y)
+----
 +Fixed+ :
 * 13/08/18:
-** *bold* inside *bullet* points // (!) Asterisk is unusable & replaced by ¨
+** *bold* inside *bullet* points
 ** padding inside the renderer
 
-*Known issues :*
+Known issues :
+* Link to username (WIP)
+* Anchor not supported yet
+* Sublists not supported (#*)
+* Formatted block not supported (bq.)
 * -Bold text with bullet points-
 * -Too much padding between elements- `
 
